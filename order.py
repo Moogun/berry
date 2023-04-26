07671448eccefc76ef1581aff197a8d4434a2595
Moogun <moogunjung@gmail.com>
2023-04-26 16:51:55 +0900

    some changes

--- order.py
+++ order.py
@@ -23,19 +23,19 @@ def handle_order():
                                 OrdQty=1, # long
                                 OrdPrc=51000, # double
                                 BnsTpCode="2",
-                                OrdprcPtnCode="82",
+                                OrdprcPtnCode="00",
                                 # 00 지정가
                                 # 03 시장가
                                 # 81 시간외종가 82 시간외 단일가
                                 MgntrnCode="000",
                                 LoanDt="",
                                 OrdCndiTpCode="0")
-    print('dfs', dfs)
+    print('dfs from order', dfs)
 
-handle_order()
+# handle_order()
 def check_stocks_holding():
     dfs = xaquery.block_request("t0424", accno="20658614901", passwd="1145")
-    print('dfs', dfs)
+    print('check_stocks_holding', dfs)
 
 # hold list of candidates
 candidate = []
@@ -45,21 +45,35 @@ def e_search():
     dfs = xaquery.block_request("t1857",
                                 sRealFlag="1", # 0: 조회, 1: 실시간
                                 sSearchFlag="S",# F: file, S: Server
-                                query_index="Taketheg0001",)
+                                query_index="Taketheg0002",)
+                                # query_index="C:\\Users\\moogun\\tech\\230413_xing\che1.2x vol400 volup1.5x.ACF"
+
     global candidate
     global liveDataKey
-    candidate = dfs[0] # dfs is [df], cts_time (a tuple) df is a list
-    # print('candidate[0] type ---------- ', type(candidate[0]), '- len(candidate) - ', len(candidate),  len(candidate[0]))
-    liveDataKey = candidate.loc[0]['AlertNum']
-    print('liveDataKey', liveDataKey)
-    pd.DataFrame(candidate[1]).to_excel("candidate2.xlsx")
+    # print('e_search dfs - ', dfs)
+    # dfs is a tuple, ( [block 2], cts_time )
+
+    if len(dfs[0]) == 0:
+        print('No Outblock')
+        return
+    else:
+        # oBlock_1 = pd.DataFrame(dfs[0])
+        print('dfs[0]', dfs[0])
+
+        # print('oBlock_1 - list', type(oBlock_1))
+        # liveDataKey = oBlock_1[0][2]
+
+        # print('oBlock_1 - df', oBlock_1.loc[0]['AlertNum'] )
+
+        # pd.DataFrame(oBlock_1).loc[0]['AlertNum']
+        # print('liveDataKey', liveDataKey)
+
+        oBlock_2 = pd.DataFrame(dfs[0])
+        oBlock_2.to_excel("candidate1.xlsx")
 
 e_search()
-print('candiate[0] ', candidate[0])
-# print('candiate[0][shcode] ', candidate[0][0])
-# print('candiate[0][shcode][AlertNum] ', candidate[0][0]['AlertNum'])
 
-#
+
 # def handle_input(input_char: str) -> None:
 #     print("You pressed", input_char)
 #     match input_char:
@@ -75,3 +89,19 @@ print('candiate[0] ', candidate[0])
 # while True:
 #     input_char = input("Press a key: ")
 #     handle_input(input_char)
+
+if __name__ == "__main__":
+    # 로그인 정보
+    f = open("../account.txt", "rt")
+    lines = f.readlines()
+    id = lines[0].strip()
+    password = lines[1].strip()
+    cert = lines[2].strip()
+    f.close()
+
+    # 로그인
+    from pyxing import session
+
+    # Session
+    xasession = session.XASession()
+    xasession.login(id, password, cert, block=True)
